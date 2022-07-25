982664935845cfdea377c21d2d1e30bb9b52dd32
Ignacio Zamorano <nachozamorano94@gmail.com>
2022-07-24 21:32:59 -0400

    ADD: Se agrega QR que emite la aplicacion con el pedido
    Signed-off-by: Ignacio Zamorano <nachozamorano94@gmail.com>

--- src/views/Segment.vue
+++ src/views/Segment.vue
@@ -1,26 +1,31 @@
 <template>
-  <ion-segment selectOnFocus scrollable mode="md" v-model="stepSelected" class="segment-w"
-    @ionChange="segmentChanged($event)">
-    <div v-for="(stp, s) in step" :key="s">
-      <ion-segment-button SwipedTabs :id="'segment-button' + stp.code" mode="md" class="step-class" :value="stp.code">
-        <ion-label>{{ stp.name }}</ion-label>
-      </ion-segment-button>
+  <div v-if="!confirmOrder" class="height-max">
+    <ion-segment selectOnFocus scrollable mode="md" v-model="stepSelected" class="segment-w"
+      @ionChange="segmentChanged($event)">
+      <div v-for="(stp, s) in step" :key="s">
+        <ion-segment-button SwipedTabs :id="'segment-button' + stp.code" mode="md" class="step-class" :value="stp.code">
+          <ion-label>{{ stp.name }}</ion-label>
+        </ion-segment-button>
+      </div>
+    </ion-segment>
+    <div id="counter" class="margin-counter" @click="showDetail()">
+      <ion-badge color="primary" class="counter-element">{{counterElement()}}</ion-badge>
+      <ion-img src="/assets/icon/fast-food-outline.svg" class="logo-icon"></ion-img>
     </div>
-  </ion-segment>
-  <div id="counter" class="margin-counter" @click="showDetail()">
-    <ion-badge color="primary" class="counter-element">{{counterElement()}}</ion-badge>
-    <ion-img src="/assets/icon/fast-food-outline.svg" class="logo-icon"></ion-img>
-  </div>
-  <list-item :step-info="stepInfo" :items-list="filterList" ref="list"></list-item>
-  <div class="size-button-div index-content">
-    <ion-text class="total-amount">Monto: {{ formatPrice(totalAmount) + " " + stepInfo.typeMoney }}</ion-text>
-    <div class="button-div">
-      <ion-button @click="backClick" color="light" v-show="stepSelected != '001'" class="style-back">Volver</ion-button>
-      <ion-button @click="nextClick" color="success" v-if="stepSelected != '00' + step.length">Continuar</ion-button>
-      <ion-button @click="finishClick" color="success" :disabled="totalAmount == 0" v-else>Confirmar Pedido</ion-button>
+    <list-item :step-info="stepInfo" :items-list="filterList" ref="list"></list-item>
+    <div class="size-button-div index-content">
+      <ion-text class="total-amount">Monto: {{ formatPrice(totalAmount) + " " + stepInfo.typeMoney }}</ion-text>
+      <div class="button-div">
+        <ion-button @click="backClick" color="light" v-show="stepSelected != '001'" class="style-back">Volver</ion-button>
+        <ion-button @click="nextClick" color="success" v-if="stepSelected != '00' + step.length">Continuar</ion-button>
+        <ion-button @click="finishClick" color="success" :disabled="totalAmount == 0" v-else>Confirmar Pedido</ion-button>
+      </div>
     </div>
+    <list-item-detail :list-order="listOrder"></list-item-detail>
   </div>
-  <list-item-detail :list-order="listOrder"></list-item-detail>
+  <div v-else class="vertical-center">
+    <vue-qrcode :value="JSON.stringify(listOrder)" :options="{ width: 200 }"></vue-qrcode>
+  </div>
 </template>
 
 <script lang="ts">
@@ -28,14 +33,16 @@ import { defineComponent } from 'vue';
 import { IonSegment, IonLabel, IonSegmentButton, IonImg, IonText, IonButton, IonBadge, menuController } from '@ionic/vue';
 import ListItemDetail from './listItemDetail.vue'
 import ListItem from './ListItem.vue'
+import VueQrcode from '@chenfengyuan/vue-qrcode'
 export default defineComponent({
   inheritAttrs: false,
   name: 'segment-top',
   components: {
-    IonSegment, IonLabel, IonSegmentButton, ListItem, IonImg, IonText, IonButton, IonBadge, ListItemDetail,
+    IonSegment, IonLabel, IonSegmentButton, ListItem, IonImg, IonText, IonButton, IonBadge, ListItemDetail, VueQrcode
   },
   data() {
     return {
+      confirmOrder:false,
       totalAmount: 0,
       stepSelected: '001',
       filterList:[],
@@ -125,7 +132,7 @@ export default defineComponent({
       return "00" + step;
     },
     finishClick: function() {
-      console.log("finish");
+      this.confirmOrder=true;
     },
     nextClick: function() {
       this.stepSelected = this.nextStep(this.stepSelected);
